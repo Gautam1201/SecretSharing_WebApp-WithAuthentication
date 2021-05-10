ef449588da9e4521c31ac45c40a3d8eed03fb1c1
Gautam1201 <72904423+Gautam1201@users.noreply.github.com>
2021-05-10 20:21:51 +0530

    Level 3 security - Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app  = express();
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,12 +41,12 @@ app.route("/register")
 
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
 
     newUser.save(function(err){
       if(err) console.log(err);
-      else res.render("login");
+      else res.render("secrets");
     });
   });
 
@@ -82,7 +80,7 @@ app.route("/login")
         if(err) console.log(err);
         else{
           if(foundUser){
-            if(foundUser.password === req.body.password){
+            if(foundUser.password === md5(req.body.password)){
               res.render("secrets");
             }
           }
