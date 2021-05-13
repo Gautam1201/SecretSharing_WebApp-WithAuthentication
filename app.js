812b84c63f6167d847985bc36d6731b96218ac8d
Gautam1201 <72904423+Gautam1201@users.noreply.github.com>
2021-05-13 13:31:42 +0530

    Adding Authentication and session.

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 //jshint esversion:6
+
 require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app  = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const url = "mongodb://localhost:27017/userDB";
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.set("view engine", "ejs");
+
 
 app.get("/",function(req,res){
   res.render('home');
+});
+
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()) res.render("secrets");
+  else res.redirect("/login");
+});
+
+app.get("/logout",(req,res)=>{
+  req.logout();
+  res.redirect("/");
 });
 
 app.route("/register")
@@ -38,19 +64,17 @@ app.route("/register")
   })
 
   .post(function(req,res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-          email: req.body.username,
-          password: hash
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect("/register");
+      }else{
+        passport.authenticate("local")(req, res, function(err, user){
+          res.redirect("/secrets");
         });
-
-        newUser.save(function(err){
-          if(err) console.log(err);
-          else res.render("secrets");
-        });
-      });
+      }
     });
+  });
 
 
 app.route("/login")
@@ -61,6 +85,19 @@ app.route("/login")
 
   .post((req,res)=>{
 
+    const user = new User({
+      username : req.body.username,
+      password : req.body.password
+    });
+
+    req.login(user, function(err){
+      if(err) console.log(err);
+      else {
+          passport.authenticate("local")(req,res,()=>{
+            res.redirect("/secrets");
+          });
+      }
+    });
 
   //My method,does not work as my password is encrypted, hence condition fails.
     // User.findOne(
@@ -76,22 +113,6 @@ app.route("/login")
     //     console.log(err);
     //   }
     // );
-
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne(
-      {email: username},
-      function(err,foundUser){
-        if(err) console.log(err);
-        else{
-          if(foundUser){
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                if(result === true) res.render("secrets");
-            });
-          }
-          }
-        }
-    );
 
   });
 
